fda9c7dfed0fd58bfc6f11ec8228acb6fd2e6d0f
Kenichi Nakano <knakano0524@gmail.com>
2025-01-21 22:00:34 -0500

    Modified two sub-scripts as tested on UVA Rivanna.

--- script/sub/11_boost.sh
+++ script/sub/11_boost.sh
@@ -7,10 +7,11 @@ test -z $DIR_BUILD && exit 9
 test -z $DIR_INST  && exit 9
 test -e $DIR_INST/include/boost && exit
 
-if [ ${HOSTNAME:0:6} != udc-ba ] ; then
-    echo "Do nothing since host != udc-ba."
-    exit 0
-fi
+#if [ ${HOSTNAME:0:6} != udc-ba ] ; then
+#    echo "Do nothing since host != udc-ba."
+#    exit 0
+#fi
+exit 0 # Do nothing since UVA Rivanna has boost by default.
 
 DIR_WORK=$DIR_BUILD/boost
 mkdir -p $DIR_WORK
@@ -23,7 +24,7 @@ cd boost_1_53_0
 set +e ## We know b2 returns 1 but it seems fine.
 ./b2
 ./b2 install
-return 0
+exit 0
 
 # The version selected here is very old but is the one in SLF 7.6...
 
--- script/sub/91_genfit.sh
+++ script/sub/91_genfit.sh
@@ -22,11 +22,17 @@ cd    GenFit
 git checkout e1039-al9
 cd ../GenFit-build
 
+OPT_TEST=
+if [ ${HOSTNAME:0:6} = udc-ba ] ; then
+    echo "Set BUILD_TESTING=false at UVA Rivanna."
+    OPT_TEST='-DBUILD_TESTING=false'
+fi
+
 # The "-DINCLUDE_OUTPUT_DIRECTORY:PATH" option is needed because
 # the PHGenFitPkg package assumes the header files are in "include/GenFit/".
 # In the future it is better to modify the PHGenFitPkg package 
 # so that this special option is not needed.
-cmake ../GenFit -DLIBRARY_OUTPUT_DIRECTORY:PATH=$DIR_INST/lib64 -DINCLUDE_OUTPUT_DIRECTORY:PATH=$DIR_INST/include/GenFit -DCMAKE_INSTALL_PREFIX=$DIR_INST
+cmake ../GenFit -DLIBRARY_OUTPUT_DIRECTORY:PATH=$DIR_INST/lib64 -DINCLUDE_OUTPUT_DIRECTORY:PATH=$DIR_INST/include/GenFit -DCMAKE_INSTALL_PREFIX=$DIR_INST $OPT_TEST
 make
 make install
 
